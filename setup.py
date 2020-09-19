63581a2bf30181fa79ed2c37c7632c93d07ee1ef
riderx <martindonadieu@gmail.com>
2020-09-19 04:34:06 +0200

    fix for setup

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
-import setuptools
+from setuptools import setup, find_packages
+
+def parse_requirements(filename):
+    """ load requirements from a pip requirements file """
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+install_reqs = parse_requirements('requirements.txt')
+reqs = install_reqs
+
+setup(
     name='naas',
-    version='0.0.1',
+    version='0.0.2',
     scripts=['scripts/naas'] ,
     author="Martin Donadieu",
     author_email="martindonadieu@gmail.com",
@@ -13,7 +21,8 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/cashstory/naas",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
+    install_requires=reqs,
     classifiers=[
         "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: MIT License",
