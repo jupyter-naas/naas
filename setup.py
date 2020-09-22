276917521e2a12968adb72b41882ac90a63e169f
riderx <martindonadieu@gmail.com>
2020-09-22 02:08:27 +0200

    update new version

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='naas',
-    version='0.0.10',
+    version='0.0.11',
     scripts=['scripts/naas'],
     author="Martin Donadieu",
     author_email="martindonadieu@gmail.com",
