16d60d260dc61a7c6f3c2e87f85e3587cc7c6282
GitHub Action <action@github.com>
2020-09-30 21:12:34 +0000

    bump: version 0.5.1 → 0.5.2

--- naas/__init__.py
+++ naas/__init__.py
@@ -12,7 +12,7 @@ from .runner.proxy import encode_proxy_url
 import requests
 import os
 
-__version__ = "0.5.1"
+__version__ = "0.5.2"
 __location__ = os.getcwd()
 scheduler = Scheduler()
 secret = Secret()
