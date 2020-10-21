14866dc280fd648781f5126dcac5dbc78a1904d3
riderx <martindonadieu@gmail.com>
2020-10-21 22:12:03 +0200

    feat: add update command

--- naas/__init__.py
+++ naas/__init__.py
@@ -8,9 +8,11 @@ from .assets import Assets
 from .secret import Secret
 from .runner import Runner
 from .api import Api
+import requests
 import os
 
 __version__ = "0.10.1"
+__github_repo = "jupyter-naas/naas"
 __location__ = os.getcwd()
 scheduler = Scheduler()
 secret = Secret()
@@ -19,6 +21,29 @@ api = Api()
 assets = Assets()
 dependency = Dependency()
 notifications = Notifications()
+
+
+def version():
+    print(__version__)
+
+
+def get_last_version():
+    url = f"https://api.github.com/repos/{__github_repo}/tags"
+    response = requests.get(url, headers={"Accept": "application/vnd.github.v3+json"})
+    return response.json()[0]["name"]
+
+
+def up_to_date():
+    return get_last_version() == version()
+
+
+def auto_update():
+    if not up_to_date():
+        update()
+
+
+def update():
+    os.system("kill -s SIGKILL 6")
 
 
 def is_production():
