0023bca59d0e7943ad1eb076a5e6a91fb6823837
GitHub Action <action@github.com>
2021-01-27 15:37:55 +0000

    bump: version 0.27.7 → 0.27.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="naas",
-    version="0.27.7",
+    version="0.27.8",
     scripts=["scripts/naas"],
     author="Martin Donadieu",
     author_email="martindonadieu@gmail.com",
