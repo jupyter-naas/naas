16309a30fddabde569459a63646dfc2c8f659786
GitHub Action <action@github.com>
2021-02-17 14:36:13 +0000

    bump: version 0.33.3 → 0.33.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="naas",
-    version="0.33.3",
+    version="0.33.4",
     scripts=["scripts/naas"],
     author="Martin Donadieu",
     author_email="martindonadieu@gmail.com",
