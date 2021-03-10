7168cce057b823dff0b53edb132c0b243aff1408
GitHub Action <action@github.com>
2021-03-10 20:19:43 +0000

    bump: version 1.3.1 → 1.3.2

--- naas/runner/runner.py
+++ naas/runner/runner.py
@@ -44,7 +44,7 @@ from naas.types import (
 asyncio.set_event_loop_policy(None)
 nest_asyncio.apply()
 
-__version__ = "1.3.1"
+__version__ = "1.3.2"
 
 
 class Runner:
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="naas",
-    version="1.3.1",
+    version="1.3.2",
     scripts=["scripts/naas"],
     author="Martin Donadieu",
     author_email="martindonadieu@gmail.com",
