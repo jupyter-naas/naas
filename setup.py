6b1ea68052aaea345de5b689d8f1af61ccacefb5
GitHub Action <action@github.com>
2021-03-15 14:46:53 +0000

    bump: version 1.4.1 → 1.4.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="naas",
-    version="1.4.1",
+    version="1.4.2",
     scripts=["scripts/naas"],
     author="Martin Donadieu",
     author_email="martindonadieu@gmail.com",
