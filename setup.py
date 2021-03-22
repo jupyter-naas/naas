d7b90dceb1e06024b67148d05cb4452170158d58
GitHub Action <action@github.com>
2021-03-22 14:36:36 +0000

    bump: version 1.5.23 → 1.5.24

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ dev_dep = [
 ]
 setup(
     name="naas",
-    version="1.5.23",
+    version="1.5.24",
     scripts=["scripts/naas"],
     author="Martin Donadieu",
     author_email="martindonadieu@gmail.com",
