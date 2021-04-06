2a1ae0e2689ef03752aa3baf2f1fd991bfa389fc
GitHub Action <action@github.com>
2021-04-06 14:59:19 +0000

    bump: version 1.10.9 → 1.10.10

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ dev_dep = [
 ]
 setup(
     name="naas",
-    version="1.10.9",
+    version="1.10.10",
     author="Martin Donadieu",
     author_email="martindonadieu@gmail.com",
     license="BSD",
