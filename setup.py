26bcebab3b7c64c5c55af264d16778c5c2305ab6
GitHub Action <action@github.com>
2021-06-04 07:59:58 +0000

    bump: version 1.15.21 → 1.16.0

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ dev_dep = [
 ]
 setup(
     name="naas",
-    version="1.15.21",
+    version="1.16.0",
     author="Martin Donadieu",
     author_email="martindonadieu@gmail.com",
     license="BSD",
