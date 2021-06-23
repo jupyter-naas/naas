e4f6a4419bcf2ec8c6f537d539223477fb4b3425
Maxime Jublou <maxime@jublou.fr>
2021-06-23 11:27:51 +0200

    fix(papermill): Surround Papermill import with warning catching logic

--- naas/runner/notebooks.py
+++ naas/runner/notebooks.py
@@ -1,3 +1,4 @@
+
 from .custom_papermill import execute_notebook
 from naas.ntypes import (
     t_notebook,
@@ -17,7 +18,11 @@ from naas.ntypes import (
 from nbconvert import HTMLExporter
 from .env_var import cpath, n_env
 from sanic import response
-import papermill as pm
+# # Temporary way to remove Papermill import warnings. Shuold be fixed when reaching Papermill 2.3.4
+import warnings
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+    import papermill as pm
 import traceback
 import datetime
 import shutil
