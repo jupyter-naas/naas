80180fd5f0dd60638db913b900865cff935a897d
Maxime Jublou <maxime@jublou.fr>
2021-08-25 11:21:02 +0200

    feat(Production): Create symlink to production folder

--- naas/__init__.py
+++ naas/__init__.py
@@ -44,6 +44,12 @@ def version():
 
 n_env.version = __version__
 
+# Create Production symlink.
+try:
+    os.makedirs('/home/ftp/.naas/home/ftp', exist_ok=True)
+    os.symlink('/home/ftp/.naas/home/ftp', '⚡Production')
+except FileExistsError as e:
+    print(e)
 
 def get_last_version():
     url = f"https://api.github.com/repos/{__github_repo}/tags"
