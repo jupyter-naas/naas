5c08c08b3b34a26c65237492ed9ee0e9c84c4cbc
Maxime Jublou <maxime@jublou.fr>
2021-08-31 07:56:56 +0200

    fix(Version): manual bump

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ dev_dep = [
 ]
 setup(
     name="naas",
-    version="1.23.0b0",
+    version="1.23.0b1",
     author="Martin Donadieu",
     author_email="martindonadieu@gmail.com",
     license="BSD",
