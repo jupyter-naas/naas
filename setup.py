5d4edb01a082b30ea68e52f78c531e98210ba4b9
ｚ <9707809+xja@users.noreply.github.com>
2021-09-25 09:41:30 +0800

    Update setup.py
    fix pip install failure
    UnicodeDecodeError: 'gbk' codec can't decode byte 0xa1 in position 2714: illegal multibyte sequence

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 NDV = "0.70.9"
