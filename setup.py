a3f888d044d315f3b5838d435fb58c07bfb323a5
actions-user <41898282+github-actions[bot]@users.noreply.github.com>
2021-10-25 15:52:35 +0000

    fix(dependency): update naas_drivers 0.74.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-NDV = "0.74.0"
+NDV = "0.74.1"
 
 driver_dep = [f'naas_drivers=={NDV}']
 dev_dep = [
