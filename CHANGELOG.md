8602bf749d466d3cdcdb19a6a03b94c97a000f02
Maxime Jublou <maxime@jublou.fr>
2021-11-19 11:06:33 +0100

    feat: Add dollars on naas manager (#254)
    * feat: Feature/credit dollar (#251)
    
    * build(deps-dev): bump twine from 3.4.1 to 3.5.0 (#244)
    
    Bumps [twine](https://github.com/pypa/twine) from 3.4.1 to 3.5.0.
    - [Release notes](https://github.com/pypa/twine/releases)
    - [Changelog](https://github.com/pypa/twine/blob/main/docs/changelog.rst)
    - [Commits](https://github.com/pypa/twine/compare/3.4.1...3.5.0)
    
    ---
    updated-dependencies:
    - dependency-name: twine
      dependency-type: direct:development
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Maxime Jublou <maxime@jublou.fr>
    
    * refactor(Manager): Update front
    
    * refactoring design manager
    
    * feat(Manager): Add routes to fetch user informations (email, plan)
    
    * end refactoring design manager
    
    * Update version
    
    * update version style
    
    * feat(Update-button): Add update version button
    
    * feat(Update-button): Add logic to stop the server on update
    
    * style(Lint): Applied black and flake8
    
    * fix(Version): Add missing controllre
    
    * fix(Manager): Remove typo and default dollars entriees
    
    * fix(Tests): Runner envs
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: jravenel <jeremy.ravenel@cashstory.com>
    
    * bump: version 2.0.2 → 2.1.0b0
    
    * feat: Feature/credit dollar (#253)
    
    * build(deps-dev): bump twine from 3.4.1 to 3.5.0 (#244)
    
    Bumps [twine](https://github.com/pypa/twine) from 3.4.1 to 3.5.0.
    - [Release notes](https://github.com/pypa/twine/releases)
    - [Changelog](https://github.com/pypa/twine/blob/main/docs/changelog.rst)
    - [Commits](https://github.com/pypa/twine/compare/3.4.1...3.5.0)
    
    ---
    updated-dependencies:
    - dependency-name: twine
      dependency-type: direct:development
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Maxime Jublou <maxime@jublou.fr>
    
    * refactor(Manager): Update front
    
    * refactoring design manager
    
    * feat(Manager): Add routes to fetch user informations (email, plan)
    
    * end refactoring design manager
    
    * Update version
    
    * update version style
    
    * feat(Update-button): Add update version button
    
    * feat(Update-button): Add logic to stop the server on update
    
    * style(Lint): Applied black and flake8
    
    * fix(Version): Add missing controllre
    
    * fix(Manager): Remove typo and default dollars entriees
    
    * fix(Tests): Runner envs
    
    * style(Event-types): Change event types names
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: jravenel <jeremy.ravenel@cashstory.com>
    
    * bump: version 2.1.0b0 → 2.1.0b1
    
    * fix(Vresion): version
    
    * bump: version 2.1.0b1 → 2.1.0b2
    
    * fix(Manager): Fix links to hub/ pages
    
    * bump: version 2.1.0b2 → 2.1.0b3
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: jravenel <jeremy.ravenel@cashstory.com>
    Co-authored-by: GitHub Action <action@github.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,22 @@
+## 2.1.0b3 (2021-11-19)
+
+### Fix
+
+- **Manager**: Fix links to hub/ pages
+
+## 2.1.0b2 (2021-11-19)
+
+### Fix
+
+- **Vresion**: version
+
 ## 2.0.3 (2021-11-18)
+
+### Fix
+
+- **dependency**: update naas_drivers 0.77.0
+
+## 2.1.0b1 (2021-11-18)
 
 ### Fix
 
--- naas/runner/controllers/env.py
+++ naas/runner/controllers/env.py
@@ -2,6 +2,20 @@ from sanic.views import HTTPMethodView
 from sanic.response import json
 from naas.ntypes import t_health
 from naas.runner.env_var import n_env
+import requests
+
+
+def get_latest_version():
+    try:
+        r = requests.get("https://pypi.python.org/pypi/naas/json")
+        r.raise_for_status()
+        response = r.json()
+        version = (
+            response["urls"][0]["filename"].replace("naas-", "").replace(".tar.gz", "")
+        )
+        return version
+    except:  # noqa: E722
+        return ""
 
 
 class EnvController(HTTPMethodView):
@@ -9,9 +23,11 @@ class EnvController(HTTPMethodView):
         super(EnvController, self).__init__(*args, **kwargs)
 
     async def get(self, request):
+
         env = {
             "status": t_health,
             "version": n_env.version,
+            "latest_version": get_latest_version(),
             "NAAS_BASE_PATH": n_env.path_naas_folder,
             "NOTIFICATIONS_API": n_env.notif_api,
             "JUPYTERHUB_USER": n_env.user,
