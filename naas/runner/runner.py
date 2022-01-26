fb7a893146e5b7d8ddc79e7ac4d2d92703fa66d2
GitHub Action <action@github.com>
2022-01-26 16:57:04 +0000

    bump: version 2.1.16 → 2.1.17

--- naas/runner/runner.py
+++ naas/runner/runner.py
@@ -50,7 +50,7 @@ from naas.ntypes import (
 asyncio.set_event_loop_policy(None)
 nest_asyncio.apply()
 
-__version__ = "2.1.16"
+__version__ = "2.1.17"
 
 
 class Runner:
--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ dev_dep = [
 ]
 setup(
     name="naas",
-    version="2.1.16",
+    version="2.1.17",
     author="Maxime Jublou",
     author_email="devops@cashstory.com",
     license="BSD",
