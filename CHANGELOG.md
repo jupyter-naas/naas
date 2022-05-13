af7a2bebfff15689dc40af681e3d86bb0a047f2e
GitHub Action <action@github.com>
2022-05-13 08:44:01 +0000

    bump: version 2.3.6 → 2.3.7

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+## 2.3.7 (2022-05-13)
+
+### Fix
+
+- **dependency**: update naas_drivers 0.96.4
+
 ## 2.3.6 (2022-05-13)
 
 ### Fix
--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ dev_dep = [
 ]
 setup(
     name="naas",
-    version="2.3.6",
+    version="2.3.7",
     author="Maxime Jublou",
     author_email="devops@cashstory.com",
     license="BSD",
