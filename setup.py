96adb6dd3318db6d8cfda9dd72ce476fc2a49dd0
actions-user <41898282+github-actions[bot]@users.noreply.github.com>
2022-06-21 14:58:14 +0000

    fix(dependency): update naas_drivers 0.102.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-NDV = "0.101.2"
+NDV = "0.102.2"
 
 driver_dep = [f'naas_drivers[full]=={NDV}']
 dev_dep = [
