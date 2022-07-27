d1ec5d70baf599251e0229f06c5751ed937e2c81
GitHub Action <action@github.com>
2022-07-27 13:56:02 +0000

    bump: version 2.6.7 → 2.6.8

--- naas/__init__.py
+++ naas/__init__.py
@@ -17,7 +17,7 @@ import requests
 import os
 import sys
 
-__version__ = "2.6.7"
+__version__ = "2.6.8"
 __github_repo = "jupyter-naas/naas"
 __doc_url = "https://naas.gitbook.io/naas/"
 __canny_js = '<script>!function(w,d,i,s){function l(){if(!d.getElementById(i)){var f=d.getElementsByTagName(s)[0],e=d.createElement(s);e.type="text/javascript",e.async=!0,e.src="https://canny.io/sdk.js",f.parentNode.insertBefore(e,f)}}if("function"!=typeof w.Canny){var c=function(){c.q.push(arguments)};c.q=[],w.Canny=c,"complete"===d.readyState?l():w.attachEvent?w.attachEvent("onload",l):w.addEventListener("load",l,!1)}}(window,document,"canny-jssdk","script");</script>'  # noqa: E501
