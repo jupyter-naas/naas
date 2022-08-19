361c3b52ec08e0be85436e437fac54130e702a40
Maxime Jublou <maxime@jublou.fr>
2022-08-19 09:15:14 +0000

    fix: notifications logger errors

--- naas/runner/notifications.py
+++ naas/runner/notifications.py
@@ -67,9 +67,7 @@ class Notifications:
             print("👌 💌 Email has been sent successfully !")
         except Exception as err:
             if self.logger is not None:
-                self.logger.error(
-                    json.dumps({"id": uid, "type": "email error", "error": str(err)})
-                )
+                self.logger.error({"id": uid, "type": "email error", "error": str(err)})
             else:
                 print(err)
 
@@ -96,7 +94,7 @@ class Notifications:
         if n_env.notif_api is None:
             jsn = {"id": uid, "type": "notification error", "error": "not configured"}
             if self.logger is not None:
-                self.logger.error(json.dumps(jsn))
+                self.logger.error(jsn)
             else:
                 print(jsn)
             return jsn
@@ -160,11 +158,7 @@ class Notifications:
             return jsn
         except Exception as err:
             if self.logger is not None:
-                self.logger.error(
-                    json.dumps(
-                        {"id": uid, "type": "notification error", "error": str(err)}
-                    )
-                )
+                self.logger.error({"id": uid, "type": "notification error", "error": str(err)})
             else:
                 print(err)
 
