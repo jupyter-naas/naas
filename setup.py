a81c2aa783b41f463686a134da8c77251ad35aba
FlorentLvr <48032461+FlorentLvr@users.noreply.github.com>
2023-01-27 10:09:28 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ setup(
         "mprop==0.16.0",
         "pydash==5.1.0",
         "pyvis==0.2.1",
-        "naas_data_product",
+        "naas-data-product",
         "rich"
     ],
     classifiers=[
