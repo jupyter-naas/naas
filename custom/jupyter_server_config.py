2a812ba8b753ba8826269cfd425b298eed4238ab
Maxime Jublou <jubloum@gmail.com>
2023-10-24 11:27:28 +0200

    feat: Auto install ABI on the machine and keep it up to date

--- custom/jupyter_server_config.py
+++ custom/jupyter_server_config.py
@@ -6,6 +6,7 @@ import os
 import logging
 import threading
 import time
+from glob import glob
 
 c = get_config()
 c.ServerApp.ip = "0.0.0.0"
@@ -93,6 +94,29 @@ def naasTemplates():
         )
         time.sleep(ONE_HOUR)
 
+def get_current_commit(dir_path):
+    commit_hash = None
+    try:
+        # Run the Git command to get the current commit hash
+        commit_hash = subprocess.check_output(['git', '-C', dir_path, 'rev-parse', 'HEAD']).decode().strip()
+    except subprocess.CalledProcessError as e:
+        print(f"Failed to get current commit: {e}")
+    
+    return commit_hash
+
+def store_last_installed_version(dir_path, version, install_log_file='.last_installed_version'):
+    installed_version_file = os.path.join(dir_path, install_log_file)
+    with open(installed_version_file, 'w') as f:
+        f.write(version)
+    return installed_version_file
+        
+def get_last_installed_version(dir_path, install_log_file='.last_installed_version'):
+    last_installed_version = None
+    installed_version_file = os.path.join(dir_path, install_log_file)
+    if os.path.exists(installed_version_file):
+        with open(installed_version_file, 'r') as f:
+            last_installed_version = f.read()
+    return last_installed_version
 
 def naasABIInstaller():
     while True:
@@ -103,27 +127,47 @@ def naasABIInstaller():
                 {
                     'repository': 'https://github.com/jupyter-naas/abi.git',
                     'target': '/home/ftp/__abi__',
-                    'version': 'main',
-                    'entrypoints': ['models/__chat_plugin__.ipynb']
+                    'version': 'main'
                 }
             ]
 
             for c in config:
+                # Clone repository
                 os.system(
-                    f"git clone {c['repository']} {c['target']}|| (cd /home/ftp/.naas/awesome-notebooks && git reset --hard && git pull)"
+                    f"git clone {c['repository']} {c['target']}"
                 )
                 
+                # Get last installed version
+                last_installed_version = get_last_installed_version(c['target'])
+                print(last_installed_version)
+
+                # Reset hard and pull latest version.
                 os.system(
                     f"cd {c['target']} && git reset --hard && git checkout {c['version']} && git pull"
                 )
-                for entrypoint in c['entrypoints']:
-                    working_directory = os.path.join(c['target'], '/'.join(entrypoint.split('/')[:1]))
+                
+                # Grab current commit version
+                current_commmit = get_current_commit(c['target'])
+                print(current_commmit)
+                
+                # Check if current commit is different than last installed version.
+                # If yes then we execute all targeted notebooks.
+                if current_commmit != last_installed_version:
                     
-                    execute_cmd = f"cd {working_directory} && jupyter nbconvert --execute {entrypoint.split('/')[-1]} --to notebook --output {entrypoint.split('/')[-1]}.setup-execution.ipynb" 
-
                     os.system(
-                        execute_cmd
+                        f"cd {c['target']} && pip install --user -r requirements.txt"
                     )
+                    
+                    entrypoints = glob(os.path.join(c['target'], '**', '__plugin__.ipynb'), recursive=True)
+                    for entrypoint in entrypoints:
+                        working_directory = '/'.join(entrypoint.split('/')[:-1])
+
+                        execute_cmd = f"cd {working_directory} && papermill {entrypoint.split('/')[-1]} {entrypoint.split('/')[-1]}.setup-execution.ipynb" 
+
+                        os.system(
+                            execute_cmd
+                        )
+                    store_last_installed_version(c['target'], current_commmit)
         except Exception as e:
             logging.error(f'Exception while installing ABI', e)
         FIVE_MINUTES = 300
