520fc10b37ec56e7d705b36dee3bf27385d72281
Maxime Jublou <jubloum@gmail.com>
2024-03-19 08:59:29 +0100

    fix: Bump naas-python

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ setup(
         "pyvis==0.3.0",
         "rich",
         "tzlocal==2.1",
-        "naas-python==1.3.0"
+        "naas-python==1.7.1"
     ],
     classifiers=[
         "Programming Language :: Python :: 3.9",
